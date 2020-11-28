cef7aa35837a00a352550674bb138350848ffa2e
sri <santoshv@clinch-infotech.com>
2020-11-28 12:20:14 +0530

    js

---
+++ js/object.js
@@ -0,0 +1,13 @@
+/*var s=[
+    fName='sri',
+    lName='satya'
+
+]
+console.log(fName+" "+lName)*/
+//=====================================
+var s={
+    fName='sri',
+    lName='satya'
+
+}
+document.write(fName+" "+lName);
